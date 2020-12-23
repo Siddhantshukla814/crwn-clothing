ee2bc9bb006d435543e42d2d3e80a9f7d77af686
Siddhant Shukla <siddhantshukla.8@gmail.com>
2020-12-24 04:09:19 +0530

    homepage starring setep is complete i.e product display

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MenuItem.scss";
+
+function MenuItem({ title, imageUrl, size }) {
+  console.log(size);
+  return (
+    <div className={size + " menu-item"}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: "url(" + imageUrl + ")" }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
+
+MenuItem.defaultProps = {
+  title: "sample",
+  subtitle: "sample",
+};
+
+export default MenuItem;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Homepage.scss";
+
+import Directory from "../components/Directory";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default Homepage;
